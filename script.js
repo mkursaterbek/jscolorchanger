e664584fffcd2954470a8eff02fc9935f7f309f1
mkursaterbek <mehmetkursaterbek@hotmail.com>
2022-09-10 18:52:07 +0300

    add button

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
-let colors = [
-  "red",
-  "green",
-  "blue",
-  "yellow",
-  "grey",
-  "pink",
-  "purple",
-  "aqua",
-];
+let colors = [];
 
 const click = document.querySelector("#click");
 const info = document.querySelector("#info");
+const buttons = document.querySelector("#buttons");
+const btn = document.createElement("button");
+buttons.appendChild(btn);
+btn.innerHTML = "Add Color";
+btn.classList.add("clickStyle");
 
 click.addEventListener("click", function () {
   const random = Math.floor(Math.random() * colors.length);
-  //   info.innerHTML = "background-color:" + colors[random];
   document.body.style.backgroundColor = colors[random];
   info.textContent = "Background-Color: " + colors[random].toUpperCase();
+});
+
+btn.addEventListener("click", function addColor() {
+  const textbox = document.querySelector("#textbox");
+  if (textbox.value != "") {
+    colors.push(textbox.value);
+    textbox.value = "";
+  }
+  console.log(colors);
 });
